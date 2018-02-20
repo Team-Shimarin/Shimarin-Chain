a756199e1dbd7ed6df90374964ce4af1a3b2fd94
nozomi0966 <nozomotoitech@gmail.com>
2018-02-20 20:48:12 +0900

    need to fix

--- handler/account.go
+++ handler/account.go
@@ -7,6 +7,9 @@ import (
 	"github.com/InvincibleMan/anzu-chain/dba"
 	"github.com/InvincibleMan/anzu-chain/model"
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"log"
+	"fmt"
 )
 
 type AccountHandler struct {
@@ -22,11 +25,30 @@ func NewAccountHandler(conf *config.Config, accountAccess dba.AccountAccess) *Ac
 }
 
 func (a *AccountHandler) Register(c *gin.Context) {
-	publicKey := c.Param("publickey")
-
+	publicKey := c.PostForm("publicKey")
+	log.Print("pubkey", publicKey)
 	_, _ = model.NewAccount(publicKey)
 
 	// TODO: RedisのKVSにJSONでぶち込む
 
 	c.String(http.StatusOK, "please wait...")
+}
+
+func (a *AccountHandler) UpdateHP(c *gin.Context){
+	id := c.Query("id")
+	hp, err := strconv.Atoi(c.Query("hp"))
+
+	log.Println(c.Params, "id =", id, " hp = ", hp)
+
+	if  err != nil {
+		log.Println("strconv hp parse error ", err)
+	}
+	log.Println(c.Request.Body, "Params id=", id, " hp=", hp)
+	accoutaccsess := dba.AccountAccess{}
+	err = accoutaccsess.UpdataBalance(id, int64(hp))
+	if err != nil{
+		log.Println(err)
+	}
+
+	c.String(http.StatusOK, "updated helth point to " + fmt.Sprint(hp))
 }

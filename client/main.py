3787d5bda06715fa577bd8567e6a0524c9a52e72
nozomi0966 <nozomotoitech@gmail.com>
2018-02-22 05:50:12 +0900

    update client

--- client/main.py
+++ client/main.py
@@ -23,5 +23,9 @@ def make_request():
 
 if __name__ == "__main__":
     while(True):
-        make_request()
-        sleep(random.randrange(10, 15))
+        try:
+            print("Connect")
+            make_request()
+            sleep(random.randrange(10, 15))
+        except as e:
+            print("Failed To Connect", e)
